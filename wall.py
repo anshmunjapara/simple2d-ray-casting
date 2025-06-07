4c7a82d18801c3b26226471c4a35569904135bf9
anshmunjapara <anshmunjapara@gmail.com>
2025-06-07 11:37:20 -0600

    add wall class

---
+++ wall.py
@@ -0,0 +1,10 @@
+import pygame
+
+
+class Wall:
+    def __init__(self, x1, y1, x2, y2):
+        self.start = pygame.math.Vector2(x1, y1)
+        self.end = pygame.math.Vector2(x2, y2)
+
+    def show(self, surface):
+        pygame.draw.line(surface, (255, 255, 255), self.start, self.end, 2)
