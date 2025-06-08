35e56eaddf17a4d10863f18b22a4d5e23313ed24
anshmunjapara <anshmunjapara@gmail.com>
2025-06-08 14:07:41 -0600

    fix: polygon was not drawn properly

--- light.py
+++ light.py
@@ -7,6 +7,9 @@ from ray import Ray
 ALPHA = 0.0001
 
 
+
+
+
 class Light:
     def __init__(self, x, y):
         self.position = pygame.math.Vector2(x, y)
@@ -38,9 +41,11 @@ class Light:
         for ray in self.rays:
             ray.show(surface)
 
+        intersections = []
         for ray in self.rays:
             minDistance = float('inf')
             minPt = None
+
             for wall in walls:
                 pt = ray.cast(wall)
                 if pt:
@@ -48,8 +53,16 @@ class Light:
                     if dis < minDistance:
                         minDistance = dis
                         minPt = pt
-            if minPt:
-                print(f"{minPt} for the ray : {ray.dir}")
-                pygame.draw.aaline(ray_surf, (255, 219, 77, 150), ray.origin, minPt)
 
+            if minPt is not None:
+                intersections.append(minPt)
+                # print(f"{minPt} for the ray : {ray.dir}")
+                # pygame.draw.aaline(ray_surf, (255, 219, 77, 150), ray.origin, minPt)
+        intersections.sort(key=self.angle_from_center)
+        points = [(int(p.x), int(p.y)) for p in intersections]
+        if len(points) > 2:
+            pygame.draw.polygon(ray_surf, (245, 203, 66, 80), points)
         surface.blit(ray_surf, (0, 0))
+
+    def angle_from_center(self, p):
+        return math.atan2(p.y - self.position.y, p.x - self.position.x)
